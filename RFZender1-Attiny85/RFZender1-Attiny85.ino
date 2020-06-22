06d948f0b40ed25dc9562fcde539bc1c6e6662d2
Marcel V <19297663+M4rc3lv@users.noreply.github.com>
2020-06-22 21:06:02 +0200

    Zend korter zodat buttons sneller reageren

--- RFZender1-Attiny85/RFZender1-Attiny85.ino
+++ RFZender1-Attiny85/RFZender1-Attiny85.ino
@@ -1,10 +1,9 @@
 #include <NewRemoteTransmitter.h>
 
-#define ADRES 400
-
+#define ADRES 200
 #define ZENDER_PEN 0
 
-NewRemoteTransmitter transmitter(ADRES, ZENDER_PEN, 260, 2);
+NewRemoteTransmitter transmitter(ADRES, ZENDER_PEN, 180, 2);
 
 void setup() { 
  transmitter.sendUnit(1, true);   
